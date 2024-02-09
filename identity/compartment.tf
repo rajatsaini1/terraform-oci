d03dc2b88a3178066b740253e98b61d1b49d7d0e
ankitt17 <ankit.tripathi@televox.com>
2024-02-09 14:21:43 +0530

    changes added for ocid

--- identity/compartment.tf
+++ identity/compartment.tf
@@ -14,7 +14,7 @@ module "sub_compartments_prod" {
     oci = oci.main
   }
   compartments     = var.sub_compartments_prod
-  compartment_ocid = module.compartments.compartment_ocid[0]
+  compartment_ocid = module.compartments.compartment_ocid["Production"]
   freeform_tags    = var.sub_compartments_prod
   env              = var.prod_env
 }
@@ -25,7 +25,7 @@ module "sub_compartments_non_prod" {
     oci = oci.main
   }
   compartments     = var.sub_compartments_non_prod
-  compartment_ocid = module.compartments.compartment_ocid[1]
+  compartment_ocid = module.compartments.compartment_ocid["Non-Production"]
   freeform_tags    = var.sub_compartments_non_prod
   env              = var.non_prod_env
 }
@@ -36,7 +36,7 @@ module "sub_compartments_prod_iris" {
     oci = oci.main
   }
   compartments     = var.sub_compartments_iris
-  compartment_ocid = module.sub_compartments_prod.compartment_ocid[0]
+  compartment_ocid = module.sub_compartments_prod.compartment_ocid["IRIS"]
   freeform_tags    = var.sub_compartments_iris
   env              = var.prod_env
   app              = var.iris_app
@@ -48,7 +48,7 @@ module "sub_compartments_prod_HCPRO" {
     oci = oci.main
   }
   compartments     = var.sub_compartments_HCPRO
-  compartment_ocid = module.sub_compartments_prod.compartment_ocid[1]
+  compartment_ocid = module.sub_compartments_prod.compartment_ocid["HCPRO"]
   freeform_tags    = var.sub_compartments_HCPRO
   env              = var.prod_env
   app              = var.HCPRO_app
@@ -60,7 +60,7 @@ module "sub_compartments_non_prod_iris" {
     oci = oci.main
   }
   compartments     = var.sub_compartments_iris
-  compartment_ocid = module.sub_compartments_non_prod.compartment_ocid[0]
+  compartment_ocid = module.sub_compartments_non_prod.compartment_ocid["IRIS"]
   freeform_tags    = var.sub_compartments_iris
   env              = var.non_prod_env
   app              = var.iris_app
@@ -72,12 +72,113 @@ module "sub_compartments_non_prod_HCPRO" {
     oci = oci.main
   }
   compartments     = var.sub_compartments_HCPRO
-  compartment_ocid = module.sub_compartments_non_prod.compartment_ocid[1]
+  compartment_ocid = module.sub_compartments_non_prod.compartment_ocid["HCPRO"]
   freeform_tags    = var.sub_compartments_HCPRO
   env              = var.non_prod_env
   app              = var.HCPRO_app
 }
 
+moved {
+  from = module.compartments.oci_identity_compartment.compartment[0]
+  to   = module.compartments.oci_identity_compartment.compartment["Production"]
+}
 
+moved {
+  from = module.compartments.oci_identity_compartment.compartment[1]
+  to   = module.compartments.oci_identity_compartment.compartment["Non-Production"]
+}
 
+moved {
+  from = module.compartments.oci_identity_compartment.compartment[2]
+  to   = module.compartments.oci_identity_compartment.compartment["OracleFunctions"]
+}
 
+moved {
+  from = module.sub_compartments_prod.oci_identity_compartment.compartment[0]
+  to   = module.sub_compartments_prod.oci_identity_compartment.compartment["IRIS"]
+}
+
+moved {
+  from = module.sub_compartments_prod.oci_identity_compartment.compartment[1]
+  to   = module.sub_compartments_prod.oci_identity_compartment.compartment["HCPRO"]
+}
+
+moved {
+  from = module.sub_compartments_non_prod.oci_identity_compartment.compartment[0]
+  to   = module.sub_compartments_non_prod.oci_identity_compartment.compartment["IRIS"]
+}
+
+moved {
+  from = module.sub_compartments_non_prod.oci_identity_compartment.compartment[1]
+  to   = module.sub_compartments_non_prod.oci_identity_compartment.compartment["HCPRO"]
+}
+
+moved {
+  from = module.sub_compartments_prod_iris.oci_identity_compartment.compartment[0]
+  to   = module.sub_compartments_prod_iris.oci_identity_compartment.compartment["Network"]
+}
+
+moved {
+  from = module.sub_compartments_prod_iris.oci_identity_compartment.compartment[1]
+  to   = module.sub_compartments_prod_iris.oci_identity_compartment.compartment["Application"]
+}
+
+moved {
+  from = module.sub_compartments_prod_iris.oci_identity_compartment.compartment[2]
+  to   = module.sub_compartments_prod_iris.oci_identity_compartment.compartment["Security"]
+}
+
+moved {
+  from = module.sub_compartments_non_prod_iris.oci_identity_compartment.compartment[0]
+  to   = module.sub_compartments_non_prod_iris.oci_identity_compartment.compartment["Network"]
+}
+
+moved {
+  from = module.sub_compartments_non_prod_iris.oci_identity_compartment.compartment[1]
+  to   = module.sub_compartments_non_prod_iris.oci_identity_compartment.compartment["Application"]
+}
+
+moved {
+  from = module.sub_compartments_non_prod_iris.oci_identity_compartment.compartment[2]
+  to   = module.sub_compartments_non_prod_iris.oci_identity_compartment.compartment["Security"]
+}
+
+moved {
+  from = module.sub_compartments_non_prod_HCPRO.oci_identity_compartment.compartment[0]
+  to   = module.sub_compartments_non_prod_HCPRO.oci_identity_compartment.compartment["Network"]
+}
+
+moved {
+  from = module.sub_compartments_non_prod_HCPRO.oci_identity_compartment.compartment[1]
+  to   = module.sub_compartments_non_prod_HCPRO.oci_identity_compartment.compartment["Application"]
+}
+
+moved {
+  from = module.sub_compartments_non_prod_HCPRO.oci_identity_compartment.compartment[2]
+  to   = module.sub_compartments_non_prod_HCPRO.oci_identity_compartment.compartment["Database"]
+}
+
+moved {
+  from = module.sub_compartments_non_prod_HCPRO.oci_identity_compartment.compartment[3]
+  to   = module.sub_compartments_non_prod_HCPRO.oci_identity_compartment.compartment["Security"]
+}
+
+moved {
+  from = module.sub_compartments_prod_HCPRO.oci_identity_compartment.compartment[0]
+  to   = module.sub_compartments_prod_HCPRO.oci_identity_compartment.compartment["Network"]
+}
+
+moved {
+  from = module.sub_compartments_prod_HCPRO.oci_identity_compartment.compartment[1]
+  to   = module.sub_compartments_prod_HCPRO.oci_identity_compartment.compartment["Application"]
+}
+
+moved {
+  from = module.sub_compartments_prod_HCPRO.oci_identity_compartment.compartment[2]
+  to   = module.sub_compartments_prod_HCPRO.oci_identity_compartment.compartment["Database"]
+}
+
+moved {
+  from = module.sub_compartments_prod_HCPRO.oci_identity_compartment.compartment[3]
+  to   = module.sub_compartments_prod_HCPRO.oci_identity_compartment.compartment["Security"]
+}
--- modules/compartments/main.tf
+++ modules/compartments/main.tf
@@ -1,9 +1,27 @@
-# create multiple compartments on oci
+# # create multiple compartments on oci
+# resource "oci_identity_compartment" "compartment" {
+#   count           = length(var.compartments)
+#   name            = var.compartments[count.index].name
+#   description     = var.compartments[count.index].desc
+#   compartment_id  = var.compartment_ocid 
+#   freeform_tags   = {(var.tag_key) = (var.compartments[count.index].name), "Env" = var.env,  "App" = var.app}
+#   #freeform_tags   = {(var.freeform_tags[count.index].tag_key) = (var.freeform_tags[count.index].name), "Env" = var.env,  "App" = var.app}
+# }
+
+locals {
+
+  compartment_names = [for item in var.compartments : [for k, v in item : v if k == "name"]]
+  compartment_desc  = [for item in var.compartments : [for k, v in item : v if k == "desc"]]
+  compartment_map   = { for item in var.compartments : item["name"] => item["desc"] }
+
+}
+
 resource "oci_identity_compartment" "compartment" {
-  count           = length(var.compartments)
-  name            = var.compartments[count.index].name
-  description     = var.compartments[count.index].desc
-  compartment_id  = var.compartment_ocid 
-  freeform_tags   = {(var.tag_key) = (var.compartments[count.index].name), "Env" = var.env,  "App" = var.app}
-  #freeform_tags   = {(var.freeform_tags[count.index].tag_key) = (var.freeform_tags[count.index].name), "Env" = var.env,  "App" = var.app}
+
+  #for_each       = toset(flatten([for item in var.compartments : [for name, desc in item : name]]))
+  for_each       = local.compartment_map
+  name           = each.key
+  description    = each.value
+  compartment_id = var.compartment_ocid
+  freeform_tags  = { (var.tag_key) = (each.key), "Env" = var.env, "App" = var.app }
 }
